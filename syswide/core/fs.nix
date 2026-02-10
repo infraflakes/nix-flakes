da6c4b4fa9d4993303fbd01b2e04491e6e2e5960
infraflakes <infraflakes@proton.com>
2026-02-10 13:08:48 +0700

    ZFS

--- syswide/core/fs.nix
+++ syswide/core/fs.nix
@@ -3,9 +3,23 @@
   pkgs,
   ...
 }: {
+#sudo zpool create -f -o ashift=12 -O compression=lz4 -O acltype=posixacl -O xattr=sa -O normalization=formD -O mountpoint=none iris_pool /dev/<disk>
+#sudo zfs create -o mountpoint=legacy iris_pool/ocis
+  boot.supportedFilesystems = [ "zfs" ];
+
+  # head -c 8 /etc/machine-id
+  networking.hostId = "e4a6f118";
+
+  # 2. Tell Nix to import this pool automatically
+  boot.zfs.extraPools = [ "iris_pool" ];
+
+  # 3. Define the mount point
   fileSystems."/data" = {
-    device = "/dev/disk/by-uuid/03aa78a4-848f-41ed-aa80-6e435a54932a";
-    fsType = "ext4";
-    options = ["nofail" "user"];
+    device = "iris_pool/ocis";
+    fsType = "zfs";
+    options = [ "nofail"]; 
   };
+  
+  services.zfs.autoScrub.enable = true;
+  services.zfs.autoSnapshot.enable = true;
 }
