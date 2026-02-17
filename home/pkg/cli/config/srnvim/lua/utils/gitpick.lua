154a3031eca5e97684350d64192997e98d0f59a7
infraflakes <infraflakes@proton.me>
2026-02-17 20:51:36 +0700

    Optimize neovim git picker speed and efficiency

--- home/pkg/cli/config/srnvim/lua/utils/gitpick.lua
+++ home/pkg/cli/config/srnvim/lua/utils/gitpick.lua
@@ -1,22 +1,23 @@
--- Find git repositories and cd into them
 _G.OpenGitRepoPicker = function()
   local actions = require "telescope.actions"
   local action_state = require "telescope.actions.state"
   local pickers = require "telescope.pickers"
   local finders = require "telescope.finders"
-  local sorters = require("telescope.config").values
+  local conf = require("telescope.config").values
 
-  local repos = {}
-  -- Reverted to the original, working find command with io.popen
-  local cmd = [[find $HOME -type d -name ".git" -prune -not -path "$HOME/.*" -not -path "$HOME/.*/**" 2>/dev/null]]
-  local handle = io.popen(cmd)
-  if handle then
-    local result = handle:read "*a"
-    handle:close()
-    for line in string.gmatch(result, "[^\r\n]+") do
-      table.insert(repos, (line:gsub("/%.git$", "")))
-    end
+  local cmd = { "fd", "-t", "d", "-H", "-I", "-g", ".git", os.getenv "HOME", "--prune" }
+  local output = vim.fn.systemlist(cmd)
+
+  if vim.v.shell_error ~= 0 then
+    vim.notify("fd command failed. Is fd installed?", vim.log.levels.ERROR)
+    return
   end
+
+  -- Explicitly remove /.git suffix, handling potential trailing slash
+  local repos = vim.tbl_map(function(git_dir)
+    -- Remove trailing slash if present, then remove /.git
+    return git_dir:gsub("/?$", ""):gsub("/%.git$", "")
+  end, output)
 
   if #repos == 0 then
     vim.notify("No Git repositories found.", vim.log.levels.WARN)
@@ -26,14 +27,25 @@ _G.OpenGitRepoPicker = function()
   pickers
     .new({}, {
       prompt_title = "Git Repositories",
-      finder = finders.new_table { results = repos },
-      sorter = sorters.file_sorter {},
+      finder = finders.new_table {
+        results = repos,
+        entry_maker = function(entry)
+          return {
+            value = entry,
+            display = vim.fn.fnamemodify(entry, ":~"),
+            ordinal = entry,
+          }
+        end,
+      },
+      sorter = conf.file_sorter {},
       attach_mappings = function(prompt_bufnr)
         actions.select_default:replace(function()
           local selection = action_state.get_selected_entry()
           actions.close(prompt_bufnr)
-          vim.cmd("cd " .. vim.fn.fnameescape(selection[1]))
-          vim.notify("cd " .. selection[1])
+          -- Debug: print what we're actually cd'ing to
+          -- print("DEBUG: cd to:", selection.value)
+          vim.cmd.cd(selection.value)
+          vim.notify("cd " .. vim.fn.fnamemodify(selection.value, ":~"))
         end)
         return true
       end,
@@ -41,5 +53,4 @@ _G.OpenGitRepoPicker = function()
     :find()
 end
 
--- Register Vim command
-vim.cmd [[ command! OpenGitRepos lua _G.OpenGitRepoPicker() ]]
+vim.api.nvim_create_user_command("OpenGitRepos", _G.OpenGitRepoPicker, {})
